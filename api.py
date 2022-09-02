36da7fe35958133b3f4e3852652033b2b80acc03
tk421 <42457203+TK421bsod@users.noreply.github.com>
2022-09-02 08:02:25 -0700

    Update api.py

--- api.py
+++ api.py
@@ -36,7 +36,7 @@ async def compute_hash():
     a = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     n = ['1', '2', '3', '4', '5', '6', '7', '8', '0']
     for each in range(1, 9):
-        hash += random.choice([random.choice(a), random.choice(n)])
+        hash = hash + random.choice([random.choice(a), random.choice(n)])
     return hash
 
 @routes.get('/shorten')
